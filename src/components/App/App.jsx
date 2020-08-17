ba01b73793378b1f4bcbcd258270ee51880ac13a
Michael Romay <amerror@gmail.com>
2020-08-17 12:19:05 -0700

    working p5 sketch inside of react

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import Sketch from '../Sketch/Sketch';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Sketch />;
 }
-  
---
+++ src/components/Sketch/Sketch.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import p5 from 'p5';
+
+export default class Sketch extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  Sketch = (p) => {
+    let x = 100;
+    let y = 100;
+    p.setup = () => {
+      p.createCanvas(200, 200);
+    };
+
+    p.draw = () => {
+      p.background(0);
+      p.fill(255);
+      p.rect(x, y, 50, 50);
+    };
+  };
+
+  componentDidMount() {
+    this.myP5 = new p5(this.Sketch, this.myRef.current);
+  }
+
+  render() {
+    return <div ref={this.myRef}></div>;
+  }
+}
