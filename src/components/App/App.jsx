7130162f3545f6abc812b5a5c40183ac0c17a273
Jojo <32883553+jlaurentpdx@users.noreply.github.com>
2020-08-18 15:46:52 -0700

    added sketch using PoseNet
    see issues for pose tracking

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Sketch from '../Sketch/Sketch';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Sketch />
+    </>
+  );
 }
-  
---
+++ src/components/Sketch/Sketch.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import p5 from 'p5';
+import ml5 from 'ml5';
+import styles from './Sketch.css';
+
+export default class Sketch extends Component {
+  state = { loading: true };
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  Sketch = (p) => {
+    let capture;
+    let poseNet;
+    let poses = [];
+
+    p.setup = () => {
+      p.createCanvas(800,600);
+      capture = p.createCapture(p.VIDEO);
+
+      poseNet = ml5.poseNet(capture, modelReady);
+      poseNet.on('pose', function(results) {
+        poses = results;
+      });
+      capture.hide();
+    }
+
+    p.mousePressed = () => {
+      console.log(JSON.stringify(poses));
+    }
+    
+    function getDistance(pos1, pos2) {
+      return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2);
+    }
+
+    function modelReady(){
+      console.log('model loaded');
+    }
+
+    p.draw = () => {
+      p.image(capture, 0, 0, p.width, p.height);
+      p.strokeWeight(2);
+      p.stroke(255);
+
+      if (poses.length > 0) {
+        let pose = poses[0].pose;
+        let right = pose['rightWrist'];
+        let left = pose['leftWrist'];
+
+        p.noFill();
+        p.quad(right.x, right.y, right.x, right.y - getDistance(left, right), left.x, left.y - getDistance(left, right), left.x, left.y);
+
+        p.fill(255, 0, 0);
+        p.ellipse(right.x, right.y, 20);
+
+        p.fill(255, 0, 0);
+        p.ellipse(left.x, left.y, 20);
+      }
+    }
+
+  };
+
+  componentDidMount() {
+    this.myP5 = new p5(this.Sketch, this.myRef.current);
+  }
+
+  render() {
+    return (
+      <>
+        <div className={styles.Sketch} ref={this.myRef}></div>
+      </>
+    );
+  }
+}
