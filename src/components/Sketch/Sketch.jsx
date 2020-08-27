27fa3b5dcbafab3ae04fe7ba991ae5ade855bfed
Jojo <32883553+jlaurentpdx@users.noreply.github.com>
2020-08-27 13:02:06 -0700

    improved behavior of PoseNet

--- src/components/Sketch/Sketch.jsx
+++ src/components/Sketch/Sketch.jsx
@@ -9,6 +9,7 @@ export default class Sketch extends Component {
     super(props);
     this.myRef = React.createRef();
   }
+  
 
   Sketch = (p) => {
     let capture;
@@ -18,8 +19,12 @@ export default class Sketch extends Component {
     let targetRightY = 0; // These may be consolidated to a list or table later
     let targetLeftX = 0;
     let targetLeftY = 0;
-    let lerpRate = 0.2;   // lerpRate between 0 and 1 determines the easement where 0 is slowest to reach target and 1 is fastest
+    let lerpRate = 0.1;   // lerpRate between 0 and 1 determines the easing where 0 is slowest to reach target and 1 is fastest
     let segments = [];
+    let distInPixels;
+    let distX;
+    let distY;
+    let mappedDistance;
 
     class Segment {
       constructor(x, y) {
@@ -45,6 +50,10 @@ export default class Sketch extends Component {
 
       checkCollision(target) {
         this.hit = collision(target.x, target.y, 5, this.x, this.y, this.w, this.h)
+      }
+
+      checkCollisionTwoPoint(targetX, targetY) {
+        this.hit = collision(targetX, targetY, 5, this.x, this.y, this.w, this.h)
       }
     }
 
@@ -89,16 +98,14 @@ export default class Sketch extends Component {
     }
 
     p.draw = () => {
-
+      
       if (poses.length > 0) {   // Draw loop using PoseNet features needs to be in this if statement
         let pose = poses[0].pose;
         let right = pose['rightWrist'];
         let left = pose['leftWrist'];
-        targetRightX = p.lerp(targetRightX, right.x, lerpRate);
-        targetRightY = p.lerp(targetRightY, right.y, lerpRate);
-        targetLeftX = p.lerp(targetLeftX, left.x, lerpRate);
-        targetLeftY = p.lerp(targetLeftY, left.y, lerpRate);
-        
+        let scoreRight = poses[0].pose.keypoints[10].score;
+        let scoreLeft = poses[0].pose.keypoints[9].score;
+
         /*
         // Mirror the camera output: 
         p.translate(p.width, 0);
@@ -107,21 +114,22 @@ export default class Sketch extends Component {
 
         p.image(capture, 0, 0, p.width, p.height);
         p.noStroke();
+
         segments[0] = new Segment(0, 0);
         segments[1] = new Segment(p.width/3, 0);
         segments[2] = new Segment(p.width/3 * 2, 0);
         segments[3] = new Segment(0, p.height/2);
         segments[4] = new Segment(p.width/3, p.height/2);
         segments[5] = new Segment(p.width/3 * 2, p.height/2);
-
+        
         for (let i = 0; i < segments.length; i++) {
-          segments[i].checkCollision(left)
+          segments[i].checkCollisionTwoPoint(targetLeftX, targetLeftY)
           if (segments[i].hit == false) {
-            segments[i].checkCollision(right); 
-          }
+            segments[i].checkCollisionTwoPoint(targetRightX, targetRightY);
+          } 
           segments[i].display();
         }
-
+        
         p.stroke(50);
         p.line(p.width/3, 0, p.width/3, p.height);
         p.line(p.width/3 *2, 0, p.width/3 * 2, p.height);
@@ -130,16 +138,40 @@ export default class Sketch extends Component {
         p.stroke(255);
         p.strokeWeight(2);
         p.line(targetLeftX, targetLeftY, targetRightX, targetRightY);
-        
-        p.fill(255, 0, 0);
-        p.ellipse(targetRightX, targetRightY, 20);
 
-        p.fill(255, 0, 0);
-        p.ellipse(targetLeftX, targetLeftY, 20);
+        if (scoreRight > 0.3 ) {
+          targetRightX = p.lerp(targetRightX, right.x, lerpRate);
+          targetRightY = p.lerp(targetRightY, right.y, lerpRate);
+          p.fill(255, 0, 0);
+          p.ellipse(targetRightX, targetRightY, 20);
+        } else {
+          p.fill(50);
+          p.ellipse(targetRightX, targetRightY, 20);
+        }
+
+        if (scoreLeft > 0.3) { 
+          targetLeftX = p.lerp(targetLeftX, left.x, lerpRate);
+          targetLeftY = p.lerp(targetLeftY, left.y, lerpRate);
+          p.fill(255, 0, 0);
+          p.ellipse(targetLeftX, targetLeftY, 20);
+        } else {
+          p.fill(50);
+          p.ellipse(targetLeftX, targetLeftY, 20);
+        }
+
+        // Data streams
 
         // Gets distance in pixels and mapped from 0 to 1 between wrist points 
-        let distInPixels = getDistance(targetLeftX, targetRightX, targetLeftY, targetRightY);
-        let mappedDistance = p.map(distInPixels, 0, p.width, 0.0, 1.0, true);
+        distInPixels = Math.floor(getDistance(targetLeftX, targetRightX, targetLeftY, targetRightY));
+        distX = Math.floor(targetLeftX - targetRightX);
+        distY = Math.floor(Math.abs(targetLeftY - targetRightY));
+        mappedDistance = p.map(distInPixels, 0, p.width, 0.0, 1.0, true);
+
+        console.log(`Distance between wrists at x-axis: ${distX}`)
+        console.log(`Distance between wrists at y-axis: ${distY}`)
+        console.log(`Total distance between wrists: ${distInPixels}`)
+
+        return distX;
       }
     }
   };
@@ -151,7 +183,9 @@ export default class Sketch extends Component {
   render() {
     return (
       <>
-        <div className={styles.Sketch} ref={this.myRef}></div>
+        <div className={styles.Sketch} ref={this.myRef}>
+          <h1></h1>
+        </div>
       </>
     );
   }
