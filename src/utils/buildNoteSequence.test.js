811483123b783704d5a56b8abd0068ef55f31906
Ben Beekman <bbeekman@gmail.com>
2020-09-03 14:17:07 -0700

    fixes for buildNoteSequence tests

--- src/utils/buildNoteSequence.test.js
+++ src/utils/buildNoteSequence.test.js
@@ -5,29 +5,29 @@ const {
 describe('tests for makeNotesFromSegmentData function', () => {
   it('should return no notes for 0 hits', () => {
     const hits = [0, 0, 0, 0, 0, 0];
-    expect(makeNotesFromSegmentData(hits)).toEqual({ 'notes': [], 'quantizationInfo': { 'stepsPerQuarter': 1 }, 'totalQuantizedSteps': 3 });
+    expect(makeNotesFromSegmentData(hits)).toEqual({ 'notes': [], 'quantizationInfo': { 'stepsPerQuarter': 4 }, 'totalQuantizedSteps': 0 });
   });
 
   it('should return a note for 1 hits', () => {
     const hits = [0, 1, 0, 0, 0, 0];
     expect(makeNotesFromSegmentData(hits)).toEqual({
       'notes': [{
-        'pitch': 65,
+        'pitch': 64,
         'quantizedEndStep': 1,
         'quantizedStartStep': 0,
       }],
-      'quantizationInfo': { 'stepsPerQuarter': 1 }, 'totalQuantizedSteps': 3
+      'quantizationInfo': { 'stepsPerQuarter': 4 }, 'totalQuantizedSteps': 1
     });
   });
   it('should return a double-duration note for 2 hits in one segment', () => {
     const hits = [0, 2, 0, 0, 0, 0];
     expect(makeNotesFromSegmentData(hits)).toEqual({
       'notes': [{
-        'pitch': 65,
+        'pitch': 64,
         'quantizedEndStep': 2,
         'quantizedStartStep': 0,
       }],
-      'quantizationInfo': { 'stepsPerQuarter': 1 }, 'totalQuantizedSteps': 3
+      'quantizationInfo': { 'stepsPerQuarter': 4 }, 'totalQuantizedSteps': 2
     });
   });
 });
