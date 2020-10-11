90edfc7db28f67a87bda790e51a0f3f01e2c1555
Ben Beekman <bbeekman@gmail.com>
2020-10-10 20:19:37 -0700

    play/pause button positioning and sizing

--- src/components/Synth/PlayControl/PlayControl.jsx
+++ src/components/Synth/PlayControl/PlayControl.jsx
@@ -5,14 +5,12 @@ export const PlayControl = ({ isPlaying, onClick }) => {
 
   const Pause = ({ onClick }) => {
     return (
-      <section className={styles.PlayControl}>
-        <label>Pause
-          <svg className="button" viewBox="0 0 60 60" onClick={onClick}>
-            <polygon points="0,0 15,0 15,60 0,60" />
-            <polygon points="25,0 40,0 40,60 25,60" />
-          </svg>
-        </label>
-      </section>
+      <label>Pause
+        <svg className="button" viewBox="0 0 60 60" onClick={onClick}>
+          <polygon points="0,0 15,0 15,60 0,60" />
+          <polygon points="25,0 40,0 40,60 25,60" />
+        </svg>
+      </label>
     );
   };
 
@@ -21,7 +19,7 @@ export const PlayControl = ({ isPlaying, onClick }) => {
       <>
         <label>Play
           <svg className="button" viewBox="0 0 60 60" onClick={onClick}>
-            <polygon points="0,0 50,30 0,60" />
+            <polygon points="0,0 60,30 0,60" />
           </svg>
         </label>
 
@@ -30,12 +28,14 @@ export const PlayControl = ({ isPlaying, onClick }) => {
   };
 
   const Toggler = ({ onClick }) => {
-    if (isPlaying) return (<Play onClick={onClick}/>);
-    else return (<Pause onClick={onClick}/>);
+    if (isPlaying) return (<Play onClick={onClick} />);
+    else return (<Pause onClick={onClick} />);
   };
 
   return (
-    <Toggler onClick={onClick} />
+    <section className={styles.PlayControl}>
+      <Toggler onClick={onClick} />
+    </section>
   );
 };
 
